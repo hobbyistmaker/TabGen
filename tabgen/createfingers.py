ccac3ab018ce3b1d74554027406c7f36788e0603
Hobbyist Maker <pahobbyistmaker@gmail.com>
2019-04-09 16:58:18 -0400

    continue refactoring

--- tabgen/createfingers.py
+++ tabgen/createfingers.py
@@ -16,9 +16,9 @@ executionFailedMsg = 'TabGen execution failed: {}'
 # Actually lay out and cut the slots to make the tabs on the selected faces.
 # It is assumed that the inputs have all been validated.
 #
-# tabWidth:     The width each tab and gap should be (mm).
+# tab_width:    The width each tab and gap should be (mm).
 # mtlThick:     The thickness of the material being cut (mm).
-# startWithTab: True if the margins (the areas at ends of the face) are to be
+# start_tab:    True if the margins (the areas at ends of the face) are to be
 #               tabs; False if they are to be cut away.
 # faces:        A list of BRepFace objects representing the selected faces.
 
@@ -28,31 +28,31 @@ design = Design.cast(product)
 root = design.rootComponent
 
 
-def create_fingers(fingerType, tabWidth, tabWidthEx, mtlThick, mtlThickEx, startWithTab, faces, app, ui=None):
+def create_fingers(finger_type, tab_width, mtlThick, start_tab, faces, app, ui=None):
     try:
-        faceLen = []
-        faceWid = []
+        face_length = []
+        face_width = []
         vert = []
         sketch = []
-        nFaces = len(faces)
+        face_count = len(faces)
 
         # For each selected face, determine face length, width, orientation,
         # origin, margin and number of tabs
-        for i in range(nFaces):
+        for i in range(face_count):
             pRange = faces[i].evaluator.parametricRange()
 
             vert.append(False)
-            faceLen.append(pRange.maxPoint.x - pRange.minPoint.x)
-            faceWid.append(pRange.maxPoint.y - pRange.minPoint.y)
+            face_length.append(pRange.maxPoint.x - pRange.minPoint.x)
+            face_width.append(pRange.maxPoint.y - pRange.minPoint.y)
             if (pRange.maxPoint.y > pRange.maxPoint.x):
                 vert[i] = True
-                t = faceLen[i]
-                faceLen[i] = faceWid[i]
-                faceWid[i] = t
+                t = face_length[i]
+                face_length[i] = face_width[i]
+                face_width[i] = t
 
         # Create a sketch on each selected face that creates the profiles we
         # need to cut the gaps between the tabs
-        for i in range(nFaces):
+        for i in range(face_count):
             # Create a new sketch on face[i].
             sketch.append(faces[i].body.parentComponent.sketches.add(faces[i]))
 
@@ -68,8 +68,6 @@ def create_fingers(fingerType, tabWidth, tabWidthEx, mtlThick, mtlThickEx, start
                 item = sketch[i].sketchPoints.item(j)
                 ix = item.geometry.x
                 iy = item.geometry.y
-                # tox = o.x
-                # toy = o.y
 
                 if ((ix < o.x and ix >= 0)
                    or (ix > o.x and ix <= 0)):
@@ -81,25 +79,26 @@ def create_fingers(fingerType, tabWidth, tabWidthEx, mtlThick, mtlThickEx, start
                     ydir = -1 if (iy > o.y and iy <= 0) else 1
                     o.y = iy
 
-                # uimessage(ui, '{}, {}, {}, {}, {}, {}'.format(tox, toy, ix, iy, xdir, ydir))
-
-            # uimessage(ui, '{}, {}, {}, {}, {}, {}'.format(o.x, o.y, mtlThick, tabWidth, xdir, ydir))
-
             xLen = 0
-            if fingerType == 'User-Defined Width':
-                nTabs = int(faceLen[i] // (2 * tabWidth))
-                margin = (faceLen[i] - 2 * tabWidth * nTabs + tabWidth) / 2
-                xLen = faceLen[i] - margin*2 - tabWidth
-            elif fingerType == 'Automatic Width':
-                default_finger_count = max(3, math.floor(faceLen[i] / tabWidth))
-                default_tab_width = faceLen[i] / default_finger_count
+            if finger_type == 'User-Defined Width':
+                default_tab_count = int(face_length[i] // (2 * tab_width))
+                margin = (face_length[i] - 2 * tab_width * default_tab_count + tab_width) / 2
+                xLen = face_length[i] - margin*2 - tab_width
+                extrude_count = default_tab_count
+            elif finger_type == 'Automatic Width':
+                default_finger_count = max(3, math.floor(face_length[i] / tab_width))
+                default_tab_width = face_length[i] / default_finger_count
                 default_notch_count = math.floor(default_finger_count/2)
-                xLen = (default_finger_count - 3)*default_tab_width
 
-                tabWidth = default_tab_width
-                margin = tabWidth
-                nTabs = default_notch_count
+                if start_tab:
+                    extrude_count = default_notch_count
+                    xLen = (default_finger_count - 3)*default_tab_width
+                else:
+                    extrude_count = default_finger_count - default_notch_count
+                    xLen = (default_finger_count - 1)*default_tab_width
 
+                tab_width = default_tab_width
+                margin = tab_width
 
             if vert[i]:
                 o.y += (margin * ydir)
@@ -113,26 +112,14 @@ def create_fingers(fingerType, tabWidth, tabWidthEx, mtlThick, mtlThickEx, start
             distance = adsk.core.ValueInput.createByReal(-mtlThick)
 
             lines.addTwoPointRectangle(o, adsk.core.Point3D.create(
-                o.x + (mtlThick if vert[i] else tabWidth)*xdir,
-                o.y + (tabWidth if vert[i] else mtlThick)*ydir,
+                o.x + (mtlThick if vert[i] else tab_width)*xdir,
+                o.y + (tab_width if vert[i] else mtlThick)*ydir,
                 o.z
                 )
             )
 
-            # # Create the profiles we need for the tabs and gaps
-            # for j in range(nTabs):
-            #     lines.addTwoPointRectangle(o, adsk.core.Point3D.create(
-            #         o.x + (mtlThick if vert[i] else tabWidth),
-            #         o.y + (tabWidth if vert[i] else mtlThick),
-            #         o.z))
-            #     # update origin for next tab
-            #     if vert[i]:
-            #         o.y += 2 * tabWidth
-            #     else:
-            #         o.x += 2 * tabWidth
-
-        # Cut the gaps between tabs on each selected face
-        for i in range(nFaces):
+        # Cut the notches between tabs on each selected face
+        for i in range(face_count):
             # Collect and then sort all the profiles we created
             pList = [sketch[i].profiles.item(j)
                      for j in range(sketch[i].profiles.count)]
@@ -143,13 +130,12 @@ def create_fingers(fingerType, tabWidth, tabWidthEx, mtlThick, mtlThickEx, start
                 pList.sort(key=lambda profile: profile.boundingBox.minPoint.x,
                            reverse=True)
 
-            # Select the ones we want to use to make the gaps
+            # Select the profile we want to use to make the nothces
             profs = adsk.core.ObjectCollection.create()
-            for j in range(1 if startWithTab else 0, len(pList), 2):
-                profs.add(pList[j])
+            profs.add(pList[1 if start_tab else 0])
 
             parent = faces[i].body.parentComponent
-            # Cut the gaps. Do it in one operation to keep the timeline neat
+            # Cut the notches. Do it in one operation to keep the timeline neat
             extrudes = faces[i].body.parentComponent.features.extrudeFeatures
             finger = extrudes.addSimple(
                 profs,
@@ -159,7 +145,7 @@ def create_fingers(fingerType, tabWidth, tabWidthEx, mtlThick, mtlThickEx, start
             inputEntities = adsk.core.ObjectCollection.create()
             inputEntities.add(finger)
 
-            xQuantity = adsk.core.ValueInput.createByReal(nTabs)
+            xQuantity = adsk.core.ValueInput.createByReal(extrude_count)
             xDistance = adsk.core.ValueInput.createByReal(xLen)
 
             rectangularPatterns = faces[i].body.parentComponent.features.rectangularPatternFeatures
@@ -171,8 +157,6 @@ def create_fingers(fingerType, tabWidth, tabWidthEx, mtlThick, mtlThickEx, start
                                                                           adsk.fusion.PatternDistanceType.ExtentPatternDistanceType)
 
             pattern = rectangularPatterns.add(rectangularPatternInput)
-            # pattern.quantityOne.expression = 'floor(max(3; ( floor(ceil(length / fingerw) / 2) * 2 ) - 1))/2'
-            # pattern.distanceOne.expression = 'length - fingerw*3'
 
         return True
 
