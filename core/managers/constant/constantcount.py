e1206db656429a68839108552616634faa156933
Hobbyist Maker <pahobbyistmaker@gmail.com>
2019-06-06 12:46:22 -0400

    temp. remove option for constant count fingers

--- core/managers/constant/constantcount.py
+++ core/managers/constant/constantcount.py
@@ -3,7 +3,7 @@ from math import ceil, floor
 
 from adsk.core import ValueInput as vi
 
-from ... import fusion
+from .. import fusion
 
 Property = namedtuple('Property', ['name', 'value', 'expression', 'comment'])
 
@@ -39,17 +39,21 @@ class Fingers:
         self.distance = self._get_distance(inputs.distance)
         self.margin = self._get_margin(inputs.margin)
         self.interior = self._get_interior(inputs.interior)
+        self.edge_margin = self._get_edge_margin(inputs.edge_margin)
 
         self.adjusted_length = self._get_adjusted_length(self._name, self.face_length, self.margin)
-        self.fingers = self._get_fingers(self._name, self.adjusted_length, self.default_width)
+        self.adjusted_depth = self._get_adjusted_depth(self._name, self.depth, self.kerf)
+        self.fingers = self._get_fingers(self._name, self.adjusted_length, self.default_width, self.tab_first)
         self.finger_length = self._get_finger_length(self._name, self.adjusted_length, self.fingers)
+        self.adjusted_finger_length = self._get_adjusted_finger_length(self._name, self.finger_length, self.kerf)
         self.finger_distance = self._get_finger_distance(self._name, self.finger_length, self.fingers, self.kerf)
         self.notches = self._get_notches(self._name, self.fingers, self.tab_first)
         self.pattern_distance = self._get_pattern_distance(self._name, self.adjusted_length, self.finger_length,
-                                                           self.kerf, self.tab_first)
-        self.distance_two = self._get_distance_two(self._name, self.distance, self.depth, self.kerf)
-        self.start = self._get_start(self._name, self.margin, self.kerf, self.finger_length, self.tab_first)
-        self.offset = self._get_offset(self._name, self.margin, self.kerf, self.tab_first)
+                                                           self.tab_first)
+        self.distance_two = self._get_distance_two(self._name, self.distance, self.adjusted_depth, self.edge_margin)
+        self.offset = self._get_offset(self._name, self.margin, self.adjusted_finger_length,
+                                       self.kerf, self.tab_first)
+        self.start = self._get_start(self._name, self.offset, self.finger_length, self.kerf, self.tab_first)
 
     def _name(self, name):
         return '{}_{}'.format(self.alias, name)
@@ -81,6 +85,9 @@ class Fingers:
     def _get_kerf(self, input_):
         return self._get_param(input_, 'kerf', 'kerf applied to offset cuts', save=False)
 
+    def _get_edge_margin(self, input_):
+        return self._get_param(input_, 'edge_margin', 'offset from face to cut notches', save=False)
+
     def _get_interior(self, input_):
         return self._get_param(input_, 'interior_walls', 'number of interior walls', save=False)
 
@@ -106,11 +113,29 @@ class Fingers:
                         'adjusted length of the face without margins')
 
     @staticmethod
-    def _get_fingers(alias, adjusted_length, default_width):
-        return Property(alias('fingers'), (ceil(max(3, floor(adjusted_length.value / default_width.value))/2)*2)-1,
-                        '((ceil(max(3; floor({} / {}))/2)*2)-1)'.format(adjusted_length.name,
-                                                                        default_width.name),
-                        'total number of fingers across the jointed faces')
+    def _get_adjusted_depth(alias, depth, kerf):
+        return Property(alias('adjusted_depth'), (abs(depth.value) - abs(kerf.value/2)),
+                        '(abs({}) - abs({}/2))'.format(depth.name, kerf.name),
+                        'kerf adjusted depth of cuts')
+
+    @staticmethod
+    def _get_adjusted_finger_length(alias, finger_length, kerf):
+        return Property(alias('adjusted_finger_length'), (abs(finger_length.value) - abs(kerf.value)),
+                        '(abs({}) - abs({}))'.format(finger_length.name, kerf.name),
+                        'kerf adjusted length of notches that are cut')
+
+    @staticmethod
+    def _get_fingers(alias, adjusted_length, default_width, tab_first):
+        if tab_first:
+            return Property(alias('fingers'), (ceil(max(3, floor(adjusted_length.value / default_width.value))/2)*2)-1,
+                            '((ceil(max(3; floor({} / {}))/2)*2)-1)'.format(adjusted_length.name,
+                                                                            default_width.name),
+                            'total number of fingers across the jointed faces')
+        else:
+            return Property(alias('fingers'), (ceil(max(3, floor(adjusted_length.value / default_width.value))/2)*2)-1,
+                            '((ceil(max(3; floor({} / {}))/2)*2)-1)'.format(adjusted_length.name,
+                                                                                default_width.name),
+                            'total number of fingers across the jointed faces')
 
     @staticmethod
     def _get_finger_length(alias, adjusted_length, fingers):
@@ -120,9 +145,9 @@ class Fingers:
 
     @staticmethod
     def _get_finger_distance(alias, finger_length, fingers, kerf):
-        return Property(alias('finger_distance'), finger_length.value * fingers.value - kerf.value,
-                        '(({}*{}/1mm) - {})'.format(finger_length.name, fingers.name, kerf.name),
-                        'full distance of notch placement')
+        return Property(alias('finger_distance'), finger_length.value * fingers.value,
+                        '(({}+{})/1mm)'.format(finger_length.name, fingers.name),
+                        'nominal distance of notch placement')
 
     @staticmethod
     def _get_notches(alias, fingers, tab_first):
@@ -130,28 +155,28 @@ class Fingers:
             value = floor(fingers.value/2)
             expression = 'floor(({}/1mm)/2)'.format(fingers.name)
         else:
-            value = ceil(fingers.value/2)
-            expression = 'ceil(({}/1mm)/2)'.format(fingers.name)
+            value = floor(fingers.value/2) - 1
+            expression = 'floor(({}/1mm)/2) - 1'.format(fingers.name)
 
         return Property(alias('notches'), value, expression, 'number of notches to cut in face')
 
     @staticmethod
-    def _get_pattern_distance(alias, adjusted_length, finger_length, kerf, tab_first):
+    def _get_pattern_distance(alias, finger_distance, finger_length, tab_first):
         if tab_first:
-            value = adjusted_length.value - finger_length.value*3 + kerf.value/2
-            expression = '(({} - {}*3) + {}/2)'.format(adjusted_length.name, finger_length.name, kerf.name)
+            value = finger_distance.value - finger_length.value*3
+            expression = '(({} - {}*3))'.format(finger_distance.name, finger_length.name)
         else:
-            value = adjusted_length.value - finger_length.value + kerf.value/2
-            expression = '(({} - {}) + {}/2)'.format(adjusted_length.name, finger_length.name, kerf.name)
+            value = finger_distance.value - finger_length.value
+            expression = '(({} - {}*5))'.format(finger_distance.name, finger_length.name)
 
         return Property(alias('pattern_distance'), value, expression,
                         'distance over which to place the rectangular pattern.')
 
     @staticmethod
-    def _get_distance_two(alias, distance, depth, kerf):
+    def _get_distance_two(alias, distance, depth, edge_margin):
         if distance.value:
-            value = distance.value - depth.value + kerf.value/2
-            expression = '({} + {} + {}/2)'.format(distance.name, depth.name, kerf.name)
+            value = distance.value - depth.value - abs(edge_margin.value)*2
+            expression = '({} - {} - abs({})*2)'.format(distance.name, depth.name, edge_margin.name)
         else:
             value = 0
             expression = '0'
@@ -159,44 +184,58 @@ class Fingers:
         return Property(alias('second_distance'), value, expression, 'distance to second face.')
 
     @staticmethod
-    def _get_start(alias, margin, kerf, finger_length, tab_first):
+    def _get_start(alias, offset, finger_length, kerf, tab_first):
         if tab_first:
-            value = margin.value + finger_length.value + kerf.value/2
-            expression = '({} + {} + ({}/2))'.format(margin.name, finger_length.name, kerf.name)
+            value = offset.value + kerf.value
+            expression = '({} + {})'.format(offset.name, kerf.name)
         else:
-            value = margin.value - kerf.value/2
-            expression = '({} - ({}/2))'.format(margin.name, kerf.name)
+            value = offset.value + finger_length.value + kerf.value
+            expression = '({} + {} + {})'.format(offset.name, finger_length.name, kerf.name)
 
         return Property(alias('start'), value, expression, 'start point for first notch')
 
     @staticmethod
-    def _get_offset(alias, margin, kerf, tab_first):
+    def _get_offset(alias, margin, adjusted_finger_length, kerf, tab_first):
         if tab_first:
-            value = margin.value - kerf.value/2
-            expression = '({} - ({}/2))'.format(margin.name, kerf.name)
+            value = (margin.value + adjusted_finger_length.value) + kerf.value/2
+            expression = '({} + {}) + {}/2'.format(margin.name, adjusted_finger_length.name, kerf.name)
         else:
-            value = margin.value + kerf.value/2
-            expression = '({} + ({}/2))'.format(margin.name, kerf.name)
+            value = (margin.value + adjusted_finger_length.value) + kerf.value/2
+            expression = '({} + {}) + {}/2'.format(margin.name, adjusted_finger_length.name, kerf.name)
 
         return Property(alias('offset'), value, expression, 'offset point for start of the finger distance')
 
     def save(self, properties):
-        properties.adjusted.default_width.parameter.expression = self.default_width.expression
-        properties.adjusted.face_length.parameter.expression = self.face_length.expression
-        properties.adjusted.second_distance.parameter.expression = self.distance.expression
-        properties.finger_dimension.parameter.expression = self.finger_length.expression
-        properties.adjusted.fingers.parameter.expression = self.fingers.expression
-        properties.offset_dimension.parameter.expression = self.start.expression
-        properties.finger_cut.extentOne.distance.expression = '-{}'.format(self.depth.expression)
+        properties.sketch.isComputeDeferred = True
+        properties.default_width.parameter.expression = self.default_width.expression
+        properties.margin.parameter.expression = self.margin.expression
+        properties.face_length.parameter.expression = self.face_length.expression
+        properties.edge_margin.parameter.expression = self.edge_margin.expression
 
-        left_dimension = getattr(properties, 'left_dimension', None)
-        if left_dimension:
-            properties.left_dimension.parameter.expression = self.offset.expression
+        properties.second_distance.parameter.expression = self.distance_two.expression
+        properties.finger_dimension.parameter.expression = self.adjusted_finger_length.expression
+        properties.adjusted_finger_length.parameter.expression = self.adjusted_finger_length.expression
+        properties.finger_count.parameter.expression = self.fingers.expression
+        properties.adjusted_length.parameter.expression = self.adjusted_length.expression
+        properties.finger_length.parameter.expression = self.finger_length.expression
+        properties.adjusted_depth.parameter.expression = self.adjusted_depth.expression
+        properties.finger_cut.extentOne.distance.expression = '-{}'.format(self.adjusted_depth.name)
 
-        properties.marker.distance.parameter.expression = self.finger_distance.expression
-        properties.adjusted.distance.parameter.expression = self.adjusted_length.expression
-        properties.pattern.distance.parameter.expression = self.pattern_distance.expression
+        properties.offset_dimension.parameter.expression = self.offset.expression
+        properties.start_dimension.parameter.expression = self.start.expression
+        properties.finger_distance.parameter.expression = self.finger_distance.expression
+        properties.pattern_distance.parameter.expression = self.pattern_distance.expression
         properties.finger_pattern.distanceOne.expression = self.pattern_distance.name
         properties.finger_pattern.distanceTwo.expression = self.distance_two.expression
         properties.finger_pattern.quantityOne.expression = self.notches.expression
         properties.finger_pattern.quantityTwo.expression = '2 + {}'.format(self.interior.value)
+
+        left_dimension = getattr(properties, 'left_dimension', None)
+        if left_dimension:
+            properties.left_dimension.parameter.expression = self.offset.name
+            properties.left_dimension.parameter.name = self._name('left_corner')
+        right_dimension = getattr(properties, 'right_dimension', None)
+        if right_dimension:
+            properties.right_dimension.parameter.expression = self.offset.name
+            properties.right_dimension.parameter.name = self._name('right_corner')
+        properties.sketch.isComputeDeferred = False
