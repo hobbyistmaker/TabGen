5e6b46c24e76eae2322a616f80423d599e3bbabf
Hobbyist Maker <pahobbyistmaker@gmail.com>
2019-06-10 13:09:02 -0400

    remove unused code

--- core/fusion/face/distancebetween.py
+++ core/fusion/face/distancebetween.py
@@ -1,12 +1,4 @@
-from adsk.core import MeasureManager
-
-from ..util import trim_zeros
-
-from .vertexdistance import vertex_distance
-from .vertexdistance import point_distance
-
-
-def distance_between(brepface, brepedge):
+def distance_between(app, brepface, brepedge):
     """ For the brepedge selected by the user, find the
         minimum distance between the brepedge and this
         brepface. This minimum distance will be used
@@ -14,11 +6,7 @@ def distance_between(brepface, brepedge):
         to cut the fingers on the secondary brepface.
         """
     if brepface.geometry.isParallelToLine(brepedge.geometry):
-        this_vertices = [brepedge.startVertex,
-                         brepedge.endVertex]
-
-        return min(trim_zeros(vertex_distance(brepface.vertices,
-                                              this_vertices)))
+        return app.measureManager.measureMinimumDistance(brepface.geometry, brepedge.geometry).value
     else:
         return 0
 
